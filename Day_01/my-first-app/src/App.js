bd0a567b03bcec2f1fde5c82479ff2b18303bbd4
Morning <morning@lkhagva-erdenes-iMac.local>
2023-01-09 12:24:46 +0800

    calc

--- Day_01/my-first-app/src/App.js
+++ Day_01/my-first-app/src/App.js
@@ -1,64 +1,149 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./component/Header";
+import { useState } from "react";
+// import Header from "./component/Header";
 
+// function App() {
+//   // let random = fetch("")
+//   //   .then((res) => res.json())
+//   //   .then((res) => console.log(res));
+//   function clickHandler() {
+//     console.log("clicked");
+//   }
+
+//   //
+//   const name = "Nomin";
+//   const title = <h1>Welcome {name}</h1>;
+//   //
+//   function welcome() {
+//     return `Welcome to ${name}'s website`;
+//   }
+//   //
+//   function greeting(name) {
+//     if (name !== undefined) {
+//       return <h1 style={customStyle}>Hello {name}!</h1>;
+//     }
+//     return <h1 style={customStyle}>Hello stranger!</h1>;
+//   }
+//   //if name contains legendary --> give style
+//   let nameCust = "legendary stranger";
+//   function greenName(nameCust) {
+//     if (nameCust.split(" ")[0] === "legendary") {
+//       return <p style={customStyle1}>Hello {nameCust}</p>;
+//     }
+//     return <p style={customStyle}>Hello {nameCust}</p>;
+//   }
+//   // style
+//   const customStyle1 = {
+//     color: "white",
+//     fontSize: "50px",
+//   };
+//   const customStyle = {
+//     color: "green",
+//     fontSize: "20px",
+//   };
+//   //if name contains legendary --> give style ternary operator
+//   let custName = "legend stranger";
+//   const ternaryStyle = {
+//     fontSize: custName.split(" ")[0] == "legend" ? "70px" : "20px",
+//   };
+//   return (
+//     <div className="App">
+//       <Header />
+//       <div className="App-header">
+//         {welcome()}
+//         {title}
+//         {greeting(name)}
+//         {greenName(nameCust)}
+//         {<p style={ternaryStyle}>Hello {custName}</p>}
+//         <button onClick={clickHandler}>Click</button>
+//       </div>
+//     </div>
+//   );
+// }
+//State
+// function App() {
+//   let [x, setX] = useState(100); //useState --> hook [x,setX] --> destructure which is let something = [10,()=> {console.log('testing)}] let [a,b] = something
+//   //function
+//   function decrement() {
+//     console.log("x = ", x);
+//     setX(x - 1);
+//   }
+//   function increment() {
+//     setX(x + 1);
+//   }
+//   return (
+//     <div className="App">
+//       <button onClick={decrement}>-</button>
+//       {x}
+//       <button onClick={increment}>+</button>
+//     </div>
+//   );
+// }
+//calculator
 function App() {
-  // let random = fetch("")
-  //   .then((res) => res.json())
-  //   .then((res) => console.log(res));
-  function clickHandler() {
-    console.log("clicked");
-  }
-
-  //
-  const name = "Nomin";
-  const title = <h1>Welcome {name}</h1>;
-  //
-  function welcome() {
-    return `Welcome to ${name}'s website`;
-  }
-  //
-  function greeting(name) {
-    if (name !== undefined) {
-      return <h1 style={customStyle}>Hello {name}!</h1>;
-    }
-    return <h1 style={customStyle}>Hello stranger!</h1>;
-  }
-  //if name contains legendary --> give style
-  let nameCust = "legendary stranger";
-  function greenName(nameCust) {
-    if (nameCust.split(" ")[0] === "legendary") {
-      return <p style={customStyle1}>Hello {nameCust}</p>;
-    }
-    return <p style={customStyle}>Hello {nameCust}</p>;
-  }
-  // style
-  const customStyle1 = {
-    color: "white",
-    fontSize: "50px",
+  let [count, setCount] = useState("");
+  let [countOne, setCountOne] = useState("");
+  let [countTwo, setCountTwo] = useState("");
+  let [sum, setSum] = useState("");
+  const changeToOne = () => {
+    setCount("1");
   };
-  const customStyle = {
-    color: "green",
-    fontSize: "20px",
+  const changeToTwo = () => {
+    setCount("2");
   };
-  //if name contains legendary --> give style ternary operator
-  let custName = "legend stranger";
-  const ternaryStyle = {
-    fontSize: custName.split(" ")[0] == "legend" ? "70px" : "20px",
+  const changeToThree = () => {
+    setCount("3");
   };
+  const changeToFour = () => {
+    setCount("4");
+  };
+  const changeToFive = () => {
+    setCount("5");
+  };
+  const changeToSix = () => {
+    setCount("6");
+  };
+  const changeToSeven = () => {
+    setCount("7");
+  };
+  const changeToEight = () => {
+    setCount("8");
+  };
+  const changeToNine = () => {
+    setCount("9");
+  };
+  const changeToZero = () => {
+    setCount("0");
+  };
+  const Add = () => {
+    setCount(count + count);
+    console.log(setCount);
+  };
+  const inputChange = (e) => {
+    setSum(e.target.value);
+    console.log(setSum);
+  };
+  const equal = () => {
+    setSum();
+  };
+  console.log(count);
   return (
     <div className="App">
-      <Header />
-      <div className="App-header">
-        {welcome()}
-        {title}
-        {greeting(name)}
-        {greenName(nameCust)}
-        {<p style={ternaryStyle}>Hello {custName}</p>}
-        <button onClick={clickHandler}>Click</button>
-      </div>
+      <input value={count} onChange={inputChange} />
+      <button onClick={changeToOne}>1</button>
+      <button onClick={changeToTwo}>2</button>
+      <button onClick={changeToThree}>3</button>
+      <button onClick={changeToFour}>4</button>
+      <button onClick={changeToFive}>5</button>
+      <button onClick={changeToSix}>6</button>
+      <button onClick={changeToSeven}>7</button>
+      <button onClick={changeToEight}>8</button>
+      <button onClick={changeToNine}>9</button>
+      <button onClick={changeToZero}>0</button>
+      <button onClick={Add}>+</button>
+      <button onClick={equal}>=</button>
     </div>
   );
 }
-
 export default App;
